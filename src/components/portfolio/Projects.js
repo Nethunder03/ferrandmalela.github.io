e81dab466f2da9890951639997e9f498f86d03ed
Rock Ferrand MALELA <guyrockmalela@gmail.com>
2024-06-17 00:55:26 +0200

    Debut de composant projets

--- src/components/portfolio/Projects.js
+++ src/components/portfolio/Projects.js
@@ -1,112 +1,128 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectList from "./ProjectList";
-import logo1 from "../letters-fm.png";
 import { FaChartPie, FaLaptopCode } from 'react-icons/fa';
 
-
 class Projects extends Component {
-    state = {
-        radios: [
-            {id: 1, value: "Tout",icon: <i class='fa-solid fa-rectangle-list'></i>},
-            {id: 2, value: "Big Data",icon: <i class='fa-solid fa-database'></i>},
-            {id: 3, value: "Data Science", icon: <FaChartPie />},
-            {id: 4, value: "DevWeb", icon: <FaLaptopCode />},
-        ],
-        selectedRadio: 'Tout'
+  state = {
+    radios: [
+      { id: 1, value: "Tout", icon: <i className='fa-solid fa-rectangle-list'></i> },
+      { id: 2, value: "Big Data", icon: <i className='fa-solid fa-database'></i> },
+      { id: 3, value: "Data Science", icon: <FaChartPie /> },
+      { id: 4, value: "DevWeb", icon: <FaLaptopCode /> },
+    ],
+    selectedRadio: 'Tout',
+    projects: []
+  };
 
-        };
+  componentDidMount() {
+    this.fetchProjects();
+  }
 
-    handleRadio = (event) => {
-        let radio = event.target.value;
-        this.setState({
-            selectedRadio: radio
-        })
+  fetchProjects = async () => {
+    try {
+      const response = await fetch('https://api.github.com/users/Nethunder03/repos');
+      const data = await response.json();
+      console.log('Fetched data:', data);
+      if (Array.isArray(data)) {
+        const projectsWithImages = data.map(project => {
+          const imageUrl = `/images/projects/${project.name}.png`;
+          return {
+            ...project,
+            imageUrl
+          };
+        });
+        console.log('Projects with images:', projectsWithImages);
+        this.setState({ projects: projectsWithImages });
+        localStorage.setItem('projects', JSON.stringify(projectsWithImages)); // Mettre en cache les projets
+      } else {
+        console.error('Error: GitHub API response is not an array', data);
+      }
+    } catch (error) {
+      console.error('Error fetching the repos:', error);
+      const cachedProjects = localStorage.getItem('projects');
+      if (cachedProjects) {
+        this.setState({ projects: JSON.parse(cachedProjects) });
+      }
     }
+  };
 
-    render() {
-        let {radios, selectedRadio} = this.state;
-        return (
-            <div>
-            <div className='portfolioContent'>
-                <ul className='radioDisplay'>
-                    {
-                        radios.map((radio) => {
-                            return (
-                                <li key={radio.id}
-                                    checked={radio.value === selectedRadio}
-                                    value={radio.value}
-                                    id={radio.value}
-                                    onChange={this.handleRadio}
-                                    >
-                                    <label htmlFor={radio.value} for={radio.value}><i>{radio.icon}</i><span>{radio.value}</span></label>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-                </div>
-                <div>
-                <Container fluid className="project-section">
-                    <Container>
-                        <h1 className="project-heading">
-                        My Recent <strong className="purple">Works </strong>
-                        </h1>
-                        <p style={{ color: "white" }}>
-                        Here are a few projects I've worked on recently.
-                        </p>
-                        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-                        <Col md={4} className="project-card">
-                            <ProjectList
-                            imgPath={logo1}
-                            isBlog={false}
-                            title="Chatify"
-                            description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-                            ghLink="https://github.com/soumyajit4419/Chatify"
-                            demoLink="https://chatify-49.web.app/"
-                            />
-                        </Col>
-                        </Row>
-                        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+  handleRadio = (event) => {
+    let radio = event.target.value;
+    this.setState({
+      selectedRadio: radio
+    });
+  };
 
-                        <Col md={4} className="project-card">
-                            <ProjectList
-                            //imgPath={leaf}
-                            isBlog={false}
-                            title="Projet 1"
-                            description="Description"
-                            ghLink="https://github.com/Nethunder03"
-                            demoLink="https://plant49-ai.herokuapp.com/"
-                            />
-                        </Col>
-
-                        <Col md={4} className="project-card">
-                            <ProjectList
-                            //imgPath={emotion}
-                            isBlog={false}
-                            title="Projet 2"
-                            description="Description"
-                            ghLink="https://github.com/Nethunder03"
-                            // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-                            />
-                        </Col>
-                        </Row>
-                    </Container>
-                </Container>
-
-                <Container fluid className="project-section">
-                    <Container>
-                        <h1 className="project-heading">
-                        Mes <strong className="purple">Projets </strong>recent
-                        </h1>
-                        <p style={{ color: "white" }}>
-                        ...
-                        </p>
-                    </Container>
-                    </Container>
-                </div></div>
-        );
+  filterProjects = (projects, category) => {
+    if (category === 'Tout') {
+      return projects;
     }
+    return projects.filter(project => {
+      if (category === "Big Data" && project.topics.includes("big-data")) return true;
+      if (category === "Data Science" && project.topics.includes("data-science")) return true;
+      if (category === "DevWeb" && project.topics.includes("web-development")) return true;
+      return false;
+    });
+  };
+
+  render() {
+    let { radios, selectedRadio, projects } = this.state;
+    const filteredProjects = this.filterProjects(projects, selectedRadio) || [];
+
+    return (
+      <div>
+        <div className='portfolioContent'>
+          <ul className='radioDisplay'>
+            {
+              radios.map((radio) => (
+                <li key={radio.id}>
+                  <input
+                    type="radio"
+                    checked={radio.value === selectedRadio}
+                    value={radio.value}
+                    id={radio.value}
+                    onChange={this.handleRadio}
+                  />
+                  <label htmlFor={radio.value}>
+                    <i>{radio.icon}</i><span>{radio.value}</span>
+                  </label>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div>
+          <Container fluid className="project-section">
+            <Container>
+              <h1 className="project-heading">
+                My Recent <strong className="purple">Works </strong>
+              </h1>
+              <p style={{ color: "white" }}>
+                Here are a few projects I've worked on recently.
+              </p>
+              <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                {
+                  filteredProjects.map(project => (
+                    <Col md={4} className="project-card" key={project.id}>
+                      <ProjectList
+                        imgPath={project.imageUrl}
+                        isBlog={false}
+                        title={project.name}
+                        description={project.description}
+                        ghLink={project.html_url}
+                        demoLink={project.homepage} // Si disponible
+                      />
+                    </Col>
+                  ))
+                }
+              </Row>
+            </Container>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Projects;
